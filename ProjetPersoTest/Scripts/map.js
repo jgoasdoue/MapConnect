36376256ad71042bc4b960d9e9d40cc8d88621f3
jerome <jerome.goasdoue@gmail.com>
2017-12-26 17:42:48 +0100

    Update ignored files, TODO file Restruct HTML code Add elements on map + functions associated

--- ProjetPersoTest/Scripts/map.js
+++ ProjetPersoTest/Scripts/map.js
@@ -1,7 +1,20 @@
 ﻿/* Fonction qui ouvre une popup au clic sur le marqueur */
 function onPopupClick(e) {
-    this.bindPopup("<b>Solutec</b><br/>Coordinates : " + e.latlng).openPopup()
+    this.bindPopup("<b>Solutec</b><br/>Coordinates : (" + e.latlng.lat.toFixed(4) + ", " + e.latlng.lng.toFixed(4) + ")");
 }
+
+/* Fonction qui ajoute une popup au clic sur la carte */
+function onMapClick(e) {
+    var popup = L.popup();
+    popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map);
+}
+
+function onGSLClick(e) {
+    var popup = L.popup();
+    this.bindPopup("<b>Gare Saint Lazare</b><br/>Coordinates : (" + e.latlng.lat.toFixed(4) + ", " + e.latlng.lng.toFixed(4) + ")");
+}
+
+
 
 /* On initialiser la carte */
 var map = L.map('map', {
@@ -9,10 +22,28 @@ var map = L.map('map', {
     zoom: 13
 });
 
+map.addEventListener("click", onMapClick);
+
 /* On ajoute les tuiles qui doivent être affichées sur la carte */
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'
 }).addTo(map);
 
 /* On ajoute un marqueur sur la carte */
-var marker = L.marker([48.87430545931439, 2.32435405254364]).addTo(map).on("click", onPopupClick);
+var solutec = L.marker([48.87430545931439, 2.32435405254364]).addTo(map);
+solutec.addEventListener("click", onPopupClick);
+
+var gareSL = L.polygon([
+    [48.87585, 2.32394],
+    [48.87718, 2.32323],
+    [48.87807, 2.32532],
+    [48.87796, 2.32549],
+    [48.87747, 2.32595],
+    [48.87746, 2.32591],
+    [48.87668, 2.32668],
+    [48.877, 2.32669],
+    [48.877, 2.32683],
+    [48.8762, 2.32679]
+]).addTo(map);
+
+gareSL.addEventListener("click", onGSLClick);
