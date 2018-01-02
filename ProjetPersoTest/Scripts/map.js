a35284349cae79be5b6850a496a96d00991954de
jerome <jerome.goasdoue@gmail.com>
2018-01-02 11:48:23 +0100

    JQuery-UI integration Add used external sources and tools Add icons and JQuery-UI images Modifications of _Layout, Map and Connexion pages Add coordinates spinner and zoom slider functions to Map page Modification of styles

--- ProjetPersoTest/Scripts/map.js
+++ ProjetPersoTest/Scripts/map.js
@@ -9,18 +9,45 @@ function onMapClick(e) {
     popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map);
 }
 
+/* Fonction qui ajoute une popup au clic sur la géométrie de Gare Saint Lazare */
 function onGSLClick(e) {
     var popup = L.popup();
     this.bindPopup("<b>Gare Saint Lazare</b><br/>Coordinates : (" + e.latlng.lat.toFixed(4) + ", " + e.latlng.lng.toFixed(4) + ")");
 }
 
-/* On initialiser la carte */
-var map = L.map('map', {
-    center: [48.853, 2.35],
-    zoom: 13
+/* Définit la latitude et la longitude d'un point */
+function setLatLong() {
+    return new L.LatLng($("#lat").val(), $("#lng").val());
+}
+
+/* Positionne le centre de la carte grâce au point créé par setLatLong() */
+function position() {
+    map.panTo(setLatLong());
+}
+
+/* Mets à jour la valeur des spinners quand on se déplace sur la carte */
+function setSpinnerCoords(e) {
+    $("#lat").spinner('value', e.target.getCenter().lat);
+    $("#lng").spinner('value', e.target.getCenter().lng);
+}
+
+/* Mets à jour la valeur du slider au zoom sur la carte */
+function setSliderZoom() {
+    $("#slider-zoom").slider('value', map.getZoom());
+}
+
+/* Initialise des spinners pour les champs de latitude et longitude */
+$("#lat, #lng").spinner({
+    step: .001,
+    change: position,
+    stop: position
 });
 
-map.addEventListener("click", onMapClick);
+/* On initialiser la carte */
+var map = L.map('map', {
+    center: setLatLong(),
+    zoom: 13,
+});
 
 /* On ajoute les tuiles qui doivent être affichées sur la carte */
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -45,3 +72,16 @@ var gareSL = L.polygon([
 ]).addTo(map);
 
 gareSL.addEventListener("click", onGSLClick);
+
+$("#slider-zoom").slider({
+    min: 0,
+    max: 18,
+    value: 13,
+    slide: function (event, ui) {
+        map.setZoom(ui.value);
+    }
+});
+
+map.addEventListener("click", onMapClick);
+map.addEventListener("move", setSpinnerCoords)
+map.addEventListener("zoomend", setSliderZoom)
